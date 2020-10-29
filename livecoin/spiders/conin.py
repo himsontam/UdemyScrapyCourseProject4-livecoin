71a91246be701f1f555445ecc248d46c27a4688c
himson Tam <himsom0528@gmail.com>
2020-10-29 21:23:44 +0800

    updated script

--- livecoin/spiders/conin.py
+++ livecoin/spiders/conin.py
@@ -14,7 +14,7 @@ class CoinSpider(scrapy.Spider):
             assert(splash:go(url))
             assert(splash:wait(1))
             rur_tab = assert(splash:select_all(".filterPanelItem___2z5Gb"))
-            rur_tab[5]:mouse_click()
+            rur_tab[4]:mouse_click()
             assert(splash:wait(1))
             splash:set_viewport_full()
             return splash:html()
